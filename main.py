2f70d71cc5033bd053c76b2d2d2822dbbc9cf29e
Eesha <eeshakanala@gmail.com>
2024-09-30 05:00:51 +0000

    Created the basic foundation of the chatbox

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+print("Welcome to the Elite 101 Chatbox!")
+userName = input("Please enter your name: ")
+userAge = input("Nice to meet you " + userName + "! How old are you? ")
+print("Welcome " + userName + "! Oh, to be " + userAge + " again! How can I help you today?")
+
+def display_menu():
+    print("Please choose from the following options:")
+    print("1. Placeholder Option 1")
+    print("2. Placeholder Option 2")
+    print("3. Placeholder Option 3")
+    print("4. Exit the conversation")
+
+def user_selection():
+    user_choice = int(input("Enter the number of your choice: "))
+    if user_choice == 4:
+        print("Goodbye," + userName + "! Have a great day!")
+
+display_menu()
+user_selection()
